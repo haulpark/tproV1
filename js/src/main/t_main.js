41b1d2f7585b6448910290cc6b500b53ed2602ea
CHO KANG RAE <joh971128@gmail.com>
2021-04-02 18:48:59 +0900

    board 차트 항목 정리

--- js/src/main/t_main.js
+++ js/src/main/t_main.js
@@ -2,8 +2,9 @@
 
 (function($){
 
-
-
-
+  $('data').visualize({type:'bar'});
+  $('data').visualize({type:'area', width:'400px'});
+  $('data').visualize({type:'pie', pieLablPos : 'inside'});
+  $('data').visualize({type:'line', lineWeight : 2});
 
 });
