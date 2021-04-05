7f1e5594917cb21f35b7887890fddef1b8b90fa5
CHO KANG RAE <joh971128@gmail.com>
2021-04-05 14:57:19 +0900

    viewBox 탭 메뉴 구현

--- js/src/main/t_main.js
+++ js/src/main/t_main.js
@@ -1,10 +1,22 @@
 // t_main.js
 
-(function($){
+$(document).ready(function(){
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
 
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+});
+
+  /*
   $('data').visualize({type:'bar'});
   $('data').visualize({type:'area', width:'400px'});
   $('data').visualize({type:'pie', pieLablPos : 'inside'});
   $('data').visualize({type:'line', lineWeight : 2});
-
-});
+  */
