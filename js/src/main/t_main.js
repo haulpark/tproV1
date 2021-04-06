fc85e3ccd6a74057a1a42b773fab5f4ec06f1e1c
CHO KANG RAE <joh971128@gmail.com>
2021-04-06 18:41:11 +0900

    하단탭 해결 완료

--- js/src/main/t_main.js
+++ js/src/main/t_main.js
@@ -29,10 +29,34 @@ $(document).ready(function(){
 		var tab_id = $(this).attr('data-tab');
 
 		$('ul.side_tabs_lank li').removeClass('current');
-		$('.side_tab_content_lank_2').removeClass('current');
+		$('.side_tab_content_lank').removeClass('current');
 
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
 	});
+
+	// ============== side tab 3 ==============
+	$('ul.side_tabs_store li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.side_tabs_store li').removeClass('current');
+		$('.side_tab_content_store').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	});
+
+
+	// ============== side tab 4 ==============
+	$('ul.side_tabs_myinfo li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.side_tabs_myinfo li').removeClass('current');
+		$('.side_tab_content_myinfo').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	});
+
 
 });
