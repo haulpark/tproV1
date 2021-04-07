a23cb48dab6f73251519878daa418584435b071a
CHO KANG RAE <joh971128@gmail.com>
2021-04-07 16:38:13 +0900

    deal박스 (좌)화면 iframe 완료(반응형 x)

---
+++ js/src/trade/t_deposit.js
@@ -0,0 +1,30 @@
+//t_deposit.js
+
+(function($){
+
+  // 타이머 
+  var time = 30;//249; // -1초 (html에 04:30 띄웠음)
+  var min = " ";
+  var sec = " ";
+
+  var x = setInterval(function(){
+    min = parseInt(time/60);
+    sec = time%60;
+
+    if (min < 10) {min = "0"+ min;}
+    if (sec < 10) {sec = "0"+ sec;} // min,sec 10미만 시 앞에 0 붙이기
+
+    document.getElementById("timer").innerHTML = min + ":" + sec + ""; // min : sec
+    time--; // -1초씩 차감
+
+    
+
+    if(time < 0 ){
+      location.href="./t_confirm.html";
+    } // 4분 30초 초과 시 이동
+    },1000);
+
+
+
+
+})(jQuery);
---
+++ js/src/trade/t_matching.js
@@ -0,0 +1,30 @@
+//t_matching.js
+
+(function($){
+
+  // 타이머 
+  var time = 299; // -1초 (html에 01:00 띄웠음)
+  var min = " ";
+  var sec = " ";
+
+  var x = setInterval(function(){
+    min = parseInt(time/60);
+    sec = time%60;
+
+    if (min < 10) {min = "0"+ min;}
+    if (sec < 10) {sec = "0"+ sec;} // min,sec 10미만 시 앞에 0 붙이기
+
+    document.getElementById("timer").innerHTML = min + ":" + sec + ""; // min : sec
+    time--; // -1초씩 차감
+
+    
+
+    if(time < 0 ){
+      location.href="#";
+    } // 5분 초과 시 이동
+    },1000);
+
+
+
+
+})(jQuery);
