51689d11b76ba43df3e4e8c77c84d3687f1cb204
CHO KANG RAE <joh971128@gmail.com>
2021-04-19 17:19:41 +0900

    이메일 인증, 휴대폰 인증, 계좌 인증(진행 중)

---
+++ js/src/certi/t_accNum_certi.js
@@ -0,0 +1,41 @@
+$(function($){
+
+  // ========== 은행 박스 열고 닫기 ==========
+
+  $('.accNumBtn').click(function(e){
+    e.preventDefault();
+    $('#bankBox').show();
+  });
+
+  $('.close').click(function(e){
+    e.preventDefault();
+
+    $('#bankBox').hide();
+  });
+
+  // ========== tab Box ==========
+
+  $('ul.tab li').click(function(){
+    var activeTab = $(this).attr('data-tab');
+
+    $('ul.tab li').removeClass('current');
+    $('.tab_content').removeClass('current');
+
+    $(this).addClass('current');
+    $('#' + activeTab).addClass('current');
+  });
+
+  // 은행/증권 dl 클릭 시 은행 선택란에 들어가기 
+
+  $('.tab_content').find('li').click(function(e){
+   
+    
+    $('.accNumBtn').find('span').text("신한은행");
+
+    $('#bankBox').hide();
+    
+
+  });
+
+
+});
