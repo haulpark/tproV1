05adb3d0cd0b2ab3a04071640fa154e652e523d9
CHO KANG RAE <joh971128@gmail.com>
2021-04-21 15:02:12 +0900

    그래프 수정(2)

--- js/src/main/t_main_graph.js
+++ js/src/main/t_main_graph.js
@@ -1,6 +1,6 @@
 /* t_main_graph.js */
 
-
+/*
 (function(){
 
   var ctx = document.getElementById('myChart').getContext('2d');
@@ -41,3 +41,32 @@
 
 })(jQuery);
 
+*/
+
+window.onload = function () {
+
+    var chart = new CanvasJS.Chart("graph", {
+        animationEnabled: true,
+        theme: "light2",
+        data: [{        
+            type: "line",
+              indexLabelFontSize: 1,
+            dataPoints: [
+                { y: 450 },
+                { y: 414},
+                { y: 520, indexLabel: "\u2191",markerColor: "red", markerType: "triangle" },
+                { y: 460 },
+                { y: 450 },
+                { y: 500 },
+                { y: 480 },
+                { y: 480 },
+                { y: 410 , indexLabel: "\u2193",markerColor: "DarkSlateGrey", markerType: "cross" },
+                { y: 500 },
+                { y: 480 },
+                { y: 510 }
+            ]
+        }]
+    });
+    chart.render();
+    
+    }
